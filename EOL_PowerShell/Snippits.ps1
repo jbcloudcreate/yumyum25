3fe623fe2b4ca44682f8c241d962f2d74e0dd734
jbcloudcreate <github@dabizzle.uk>
2025-11-10 21:09:52 +0000

    Update Snippits.ps1

--- EOL_PowerShell/Snippits.ps1
+++ EOL_PowerShell/Snippits.ps1
@@ -24,3 +24,8 @@ Add-MailboxPermission -Identity "sharedmailbox@domain.com" `-User "user@domain.c
 
 # View Users with Full Access to a Shared Mailbox
 Get-MailboxPermission -Identity "sharedmailbox@domain.com" | Where-Object { $_.AccessRights -contains "FullAccess" -and -not $_.IsInherited -and $_.User -ne "NT AUTHORITY\SELF" } | Select User, AccessRights
+
+# Rules
+Get-InboxRule -Mailbox "user@domain.com" | Select Name, Enabled, Priority, From, SubjectContainsWords, MoveToFolder
+
+Get-InboxRule -Mailbox "user@domain.com" | Format-List Name, Description, Enabled, Priority, From, SentTo, SubjectContainsWords, MoveToFolder, RedirectTo, ForwardTo, DeleteMessage
