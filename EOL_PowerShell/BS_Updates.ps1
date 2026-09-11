93635ed56fb8d1d7325b8b0db68075add1198dc7
jbcloudcreate <github@dabizzle.uk>
2026-09-11 10:21:12 +0100

    Update BS_Updates.ps1

--- EOL_PowerShell/BS_Updates.ps1
+++ EOL_PowerShell/BS_Updates.ps1
@@ -53,3 +53,7 @@ $s.AuthenticateAsClient($h)
 [Security.Cryptography.X509Certificates.X509Certificate2]::new($s.RemoteCertificate) |
   Format-List Subject, NotBefore, NotAfter, Thumbprint
 $s.Dispose(); $c.Dispose()
+
+Get-ChildItem Cert:\LocalMachine -Recurse -ErrorAction SilentlyContinue |
+  Where-Object { $_.Thumbprint -eq '9BA2B2B70ECB24041D3A97DCE93A156CBC162A57' } |
+  Select-Object PSParentPath, Subject, NotAfter
