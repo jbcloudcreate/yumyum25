4bd934463cd3d9fd86aa8c380a6174c2df086663
jbcloudcreate <github@dabizzle.uk>
2026-09-11 09:37:09 +0100

    Update BS_Updates.ps1

--- EOL_PowerShell/BS_Updates.ps1
+++ EOL_PowerShell/BS_Updates.ps1
@@ -6,3 +6,10 @@ Resolve-DnsName swpdev-ictweb.swp-rest.police.int
 
 Get-WebBinding | Select-Object protocol, bindingInformation, ItemXPath
 netsh http show sslcert
+
+Get-ChildItem Cert:\LocalMachine\My |
+  Where-Object { $_.Thumbprint -eq 'F07598B26011CE01C99E492AE62DD2EAC24875B7' } |
+  Format-List Subject, DnsNameList, NotAfter, EnhancedKeyUsageList, Issuer
+
+Get-Website | Select-Object Name, State, PhysicalPath
+Get-WebApplication
