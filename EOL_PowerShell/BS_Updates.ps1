7f65c275d3416c2e304d2f3d4a4c6349c8ef34fb
jbcloudcreate <github@dabizzle.uk>
2026-09-11 13:45:54 +0100

    Update BS_Updates.ps1

--- EOL_PowerShell/BS_Updates.ps1
+++ EOL_PowerShell/BS_Updates.ps1
@@ -2,10 +2,34 @@ Get-WebConfiguration -PSPath "MACHINE/WEBROOT/APPHOST" `
   -Filter "/system.webServer/security/authentication/anonymousAuthentication" |
   Select-Object OverrideMode, OverrideModeEffective
 
-appcmd set config /section:anonymousAuthentication /overrideMode:Deny
-appcmd set config "Default Web Site/signageadmin" /section:anonymousAuthentication /overrideMode:Allow
+& "$env:windir\system32\inetsrv\appcmd.exe" set config /section:anonymousAuthentication /overrideMode:Deny
+& "$env:windir\system32\inetsrv\appcmd.exe" set config "Default Web Site/signageadmin" /section:anonymousAuthentication /overrideMode:Allow
 
-TEST
+OR
+
+Set-WebConfiguration -PSPath "MACHINE/WEBROOT/APPHOST" `
+  -Filter "/system.webServer/security/authentication/anonymousAuthentication" `
+  -Metadata overrideMode -Value Deny
+
+Set-WebConfiguration -PSPath "MACHINE/WEBROOT/APPHOST" `
+  -Location "Default Web Site/signageadmin" `
+  -Filter "/system.webServer/security/authentication/anonymousAuthentication" `
+  -Metadata overrideMode -Value Allow
+
+Verify:
+
+Get-WebConfiguration -PSPath "MACHINE/WEBROOT/APPHOST" `
+  -Filter "/system.webServer/security/authentication/anonymousAuthentication" |
+  Select-Object OverrideMode, OverrideModeEffective
+
+Get-WebConfiguration -PSPath "MACHINE/WEBROOT/APPHOST" -Location "Default Web Site/signageadmin" `
+  -Filter "/system.webServer/security/authentication/anonymousAuthentication" |
+  Select-Object OverrideMode, OverrideModeEffective
+
+TEST - Issues (reverse)
+Set-WebConfiguration -PSPath "MACHINE/WEBROOT/APPHOST" `
+  -Filter "/system.webServer/security/authentication/anonymousAuthentication" `
+  -Metadata overrideMode -Value Allow
 
 Stop-Website -Name "Signagefeedadmin"
 Stop-Website -Name "SWPICTSignageFeedHub"
